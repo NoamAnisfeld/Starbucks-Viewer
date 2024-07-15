4783c736a56e613ff52a42f6a21550dee4cffe84
Noam Anisfeld <NoamAnisfeld@gmail.com>
2024-07-15 15:38:35 +0300

    adding OpenLayer's interactive world map

---
+++ src/open-layer.ts
@@ -0,0 +1,17 @@
+import './open-layer.css';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+
+export const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
